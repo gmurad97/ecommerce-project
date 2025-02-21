b3b57621a125ee12bbf725a052c7b7b093b9947c
gmurad97 <murad.dev@bk.ru>
2025-02-21 21:53:02 +0400

    update & change template

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -5,4 +5,12 @@ app_name = "pages"
 
 urlpatterns = [
     path("", views.index, name="home"),
+
+    path("register/", views.index, name="register"),
+    path("login/", views.index, name="login"),
+    path("", views.index, name="home"),
+    path("", views.index, name="home"),
+    path("", views.index, name="home"),
+    path("", views.index, name="home"),
+    path("", views.index, name="home"),
 ]
