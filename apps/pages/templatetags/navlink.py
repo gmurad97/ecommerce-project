15bf36adf62c4bd5f0157e9141f98be5a8579138
gmurad97 <murad.dev@bk.ru>
2025-02-24 23:15:00 +0400

    logic finished (template not finished)

---
+++ apps/pages/templatetags/navlink.py
@@ -0,0 +1,19 @@
+from django import template
+from django.urls import reverse
+
+register = template.Library()
+
+
+@register.simple_tag
+def navlink(request, url_name, active_class="active", exact_match=False, **kwargs):
+    current_url = request.path
+    target_url = reverse(url_name, kwargs=kwargs)
+    return (
+        active_class
+        if (
+            current_url == target_url
+            if exact_match
+            else current_url.startswith(target_url)
+        )
+        else ""
+    )
--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -4,20 +4,16 @@ from . import views
 app_name = "pages"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-
-    path("products/", views.index, name="product_list"),
-    path("products/<slug:slug_id>/", views.index, name="product_detail"),
-    path("categories/", views.index, name="category_list"),
-    path("categories/<slug:slug_id>/", views.index, name="category_products"),
-
-
-    path("contacts/", views.index, name="index"),
-    path("about/", views.index, name="index"),
-    path("gallery/", views.index, name="index"),
-    path("login/", views.index, name="index"),
-    path("register/", views.index, name="index"),
-    
-    path("news/", views.index, name="index"),
-    path("news/<slug:slug_id>", views.index, name="index"),
+    path("", views.index, name="home"),
+    path("home/", views.home),
+    path("about/", views.about, name="about"),
+    path("contacts/", views.contacts, name="contacts"),
+    path("faq/", views.faq, name="faq"),
+    path("gallery/", views.gallery, name="gallery"),
+    path("categories/", views.category_list, name="category_list"),
+    path("categories/<slug:slug_id>", views.category_products, name="category_products"),
+    path("products/", views.product_list, name="product_list"),
+    path("products/<slug:slug_id>", views.product_detail, name="product_detail"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
 ]
